2793e04d3df853483bf17ffc788f559a5fb6e98a
xieliming2017 <xieliming2006@hotmail.com>
2018-03-20 14:03:36 -0400

    blociroff-3-user-sign-in completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
 
-  root 'welcome#index'
+  root 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
